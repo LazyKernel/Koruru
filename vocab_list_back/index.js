a494823e1fc6d6d24df89c7996fcfc04a293e9df
LazyKernel <miksu.fredriksson@hotmail.com>
2020-07-09 20:29:14 +0300

    Add explicit text

--- vocab_list_back/index.js
+++ vocab_list_back/index.js
@@ -51,8 +51,8 @@ router.get('/api/cards/search/:fromIdx&:numCards&:term', async (req, res) => {
 
     const qry = await pool.query(
         "SELECT * FROM cards " +
-        "WHERE vocab_jp LIKE '%$1%' OR vocab_en LIKE '%$1%' OR japanese LIKE '%$1%' OR english LIKE '%$1%' " +
-        "ORDER BY index, vocab_jp LIKE '%$1%', vocab_en LIKE '%$1%' " +
+        "WHERE vocab_jp LIKE '%$1::text%' OR vocab_en LIKE '%$1::text%' OR japanese LIKE '%$1::text%' OR english LIKE '%$1::text%' " +
+        "ORDER BY index, vocab_jp LIKE '%$1::text%', vocab_en LIKE '%$1::text%' " +
         "LIMIT $2::integer OFFSET $3::integer",
         [req.params.term, req.params.numCards, req.params.fromIdx]
     )
